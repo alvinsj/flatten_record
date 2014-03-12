1ba92d225cb5b68ae07242e0d84eca9102c62289
alvinsj <alvin.nsj@gmail.com>
2014-03-12 16:25:28 +0700

    remove unused class

--- lib/flatten_record.rb
+++ lib/flatten_record.rb
@@ -2,7 +2,6 @@ module FlattenRecord
   extend ActiveSupport::Concern
   extend ActiveSupport::Autoload
 
-  require 'flatten_record/attr_denormalizer'
   autoload :DenormalizerMeta, 'flatten_record/denormalizer_meta'
   autoload :Denormalizer, 'flatten_record/denormalizer'
   autoload :DenormalizerHook, 'flatten_record/denormalizer_hook'
