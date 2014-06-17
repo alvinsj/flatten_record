52e50df4ee79cb0842ba26c4a3e39d00d80833e1
alvinsj <alvin.nsj@gmail.com>
2014-06-17 16:56:33 +0700

    change observer to after_commit

--- lib/flatten_record/observer.rb
+++ lib/flatten_record/observer.rb
@@ -1,12 +1,34 @@
 module FlattenRecord
   class Observer < ActiveRecord::Observer
-    def after_save(record)
+    
+    def after_commit(record)
+      #puts "wwwww: #{record.send(:transaction_include_action?, :create)}"
+      #puts "wwwww: #{record.send(:transaction_include_action?, :update)}"
+      #puts "ggggg: #{record.send(:transaction_include_action?, :destroy)}"
+      _after_save(record) if record.send(:transaction_include_action?, :create) || record.send(:transaction_include_action?, :update)
+
+      _after_destroy(record) if record.send(:transaction_include_action?, :destroy)
+    end
+
+    def _after_save(record)
       if denormalized_model
         denormalized_model.create_denormalized(record.reload)
       end
     end
 
-    def after_destroy(record)
+    def _after_destroy(record)
+      if denormalized_model
+        denormalized_model.destroy_denormalized(record)
+      end
+    end
+
+    def _create_denormalized(record)
+      if denormalized_model
+        denormalized_model.create_denormalized(record.reload)
+      end
+    end
+
+    def _destroy_denormalized(record)
       if denormalized_model
         denormalized_model.destroy_denormalized(record)
       end
