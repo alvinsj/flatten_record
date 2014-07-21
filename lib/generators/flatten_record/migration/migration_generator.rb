6cc56224827630cf09b23eab3ddb956c7c0a605b
alvinsj <alvin.nsj@gmail.com>
2014-07-21 20:09:16 +0700

    refactor again

--- lib/generators/flatten_record/migration/migration_generator.rb
+++ lib/generators/flatten_record/migration/migration_generator.rb
@@ -11,42 +11,50 @@ module FlattenRecord
       argument :name, :description => 'path to model'
 
       def generate_files
-        if !valid? 
-          puts("Error. #{name.camelize} is not found")
-          return
+        unless valid? 
+          puts("Error. #{name.camelize} is not found") and return
         end
 
         defined_classes.each do |class_name|
           @klass = class_name.constantize
           @table_name = @klass.table_name
-          @class_name = @table_name.camelize
-          @table_columns = @klass.denormalizer_meta.denormalized_columns if @klass.denormalizer_meta
+          @table_columns = denormalized_columns if @klass.denormalizer_meta
 
           case
-          when@klass.table_exists?
-            puts "Warning. Table already exists: #{@table_name}"
+          when @klass.table_exists?
+            puts("Warning. Table already exists: #{@table_name}")
             diff_and_generate if @klass.denormalizer_meta 
+          
           when @klass.denormalizer_meta
-            migration_template 'migration.erb', "db/migrate/create_table_#{@table_name}.rb"
+            migration_template('migration.erb', "db/migrate/create_table_#{@table_name}.rb")
+          
           else
-            puts "Error. No denormalization definition found in #{class_name}"
+            puts("Error. No denormalization definition found in #{class_name}")
           end
         end
       end
 
       private
+      def denormalized_columns
+        @klass.denormalizer_meta.denormalized_columns
+      end
+      
       def diff_and_generate
-        if !add_columns.empty? || !drop_columns.empty? 
+        if any_diff?
           puts "Generating migration based on the difference.."
-
-          puts "Add columns: " + add_columns_names unless add_columns.empty?
-          puts "Drop columns: " + drop_columns_names unless drop_columns.empty?
+          puts "Add columns: #{add_columns_names}" unless add_columns.empty?
+          puts "Drop columns: #{drop_columns_names}" unless drop_columns.empty?
 
           @migration = add_columns.empty? ? 
-            "drop_"+drop_columns.first.name+"_from" : "add_"+add_columns.first.name+"_to"
+            "drop_#{drop_columns.first.name}_from" : 
+            "add_#{add_columns.first.name}_to"
           
-          migration_template 'update.erb', "db/migrate/#{@migration}_#{@table_name}.rb"
+          migration_template('update.erb', "db/migrate/#{@migration}_#{@table_name}.rb")
         end
+      end
+
+      def any_diff?
+        !add_columns.empty? || !drop_columns.empty? 
       end
 
       def add_columns_names
