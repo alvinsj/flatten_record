bd78ad4f57fdea8c43cdac973700299ddd32ca08
alvinsj <alvin.nsj@gmail.com>
2014-08-19 12:18:10 +0700

    added spec for :prefix option

--- spec/lib/flatten_record/flattener_spec.rb
+++ spec/lib/flatten_record/flattener_spec.rb
@@ -72,46 +72,6 @@ describe FlattenRecord::Flattener do
       order
     end
 
-
-    it 'should build meta correctly' do
-      meta = klass.flattener_meta
-      expect(meta).to_not be_nil 
-      
-      expect(meta.all_columns).to_not be_empty
-      column_names = meta.all_columns.map(&:name) 
-
-      expect(column_names.count).to eq(12)
-
-      expect(column_names).to be_include("order_id")
-      expect(column_names).to be_include("total")
-      expect(column_names).to be_include("customer_id")
-      expect(column_names).to be_include("grand_total")
-      expect(column_names).to be_include("total_in_usd")
-      expect(column_names).to be_include("customer_id")
-      expect(column_names).to be_include("customer_child_id")
-      expect(column_names).to be_include("customer_child_name")
-      expect(column_names).to be_include("customer_child_cat_id")
-      expect(column_names).to be_include("customer_child_cat_name")
-      expect(column_names).to be_include("customer_child_cat_owner_child_id")
-      expect(column_names).to be_include("customer_child_cat_owner_child_name")
-    end
-
-    context '.create_with' do
-      it 'should be able to create denormalized record' do
-        denormalized = klass.create_with(order_with_customer_with_one_child) 
-        expect(denormalized.count).to eq(1)
-  
-        record = denormalized.first
-        
-        expect(record.total).to eq(100)
-        expect(record.grand_total).to eq(50)
-        expect(record.total_in_usd).to eq(1000)
-        expect(record.customer_name).to eq("Alvin")
-        expect(record.customer_child_name).to eq("Ethan")
-        expect(record.customer_child_cat_name).to eq("Meow")
-      end
-    end #/.create_with
-
     context '.find_with' do
       it 'should be able to find related records' do 
         denormalized = klass.create_with(order_with_customer_with_one_child) 
